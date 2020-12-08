1ade3b616051be8d3da0803380d0ca3e379f733f
TFS-ACC <tabitha.stevens@g.austincc.edu>
2020-12-07 22:08:49 -0600

    fixing issue

--- src/roll.cpp
+++ src/roll.cpp
@@ -6,10 +6,7 @@ void Roll::roll_die(){
     Roll::die1.roll();
     Roll::die2.roll();
 
-    int die1_rolled_value = die1.rolled_value();
-    int die2_rolled_value = die2.rolled_value();
-
-    value = die1_rolled_value + die2_rolled_value;
+    value = die1.rolled_value() + die2.rolled_value();
 
     Roll::rolled = true;
 
