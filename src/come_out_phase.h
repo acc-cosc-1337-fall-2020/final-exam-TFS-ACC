e9a72e2761e123150a2c0114ee558a490fcf240e
TFS-ACC <tabitha.stevens@g.austincc.edu>
2020-12-07 20:10:57 -0600

    Question 4

--- src/come_out_phase.h
+++ src/come_out_phase.h
@@ -1,1 +1,17 @@
 //
+#include<iostream>
+#include "phase.h"
+
+#ifndef COME_OUT_PHASE_H
+#define COME_OUT_PHASE_H
+
+class ComeOutPhase : Phase 
+{
+
+    public:
+
+        RollOutcome get_outcome(Roll* roll) override;
+
+};
+
+#endif
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -3,6 +3,8 @@
 #include "die.h"
 #include "roll.h"
 #include "shooter.h"
+#include "come_out_phase.h"
+#include "point_phase.h"
 
 // TEST_CASE("Verify Test Configuration", "verification") {
 // 	REQUIRE(true == true);
@@ -14,7 +16,7 @@ TEST_CASE("Testing Die Class:"){
 
 	Die dice;
 
-	for(int i = 0; i <= 10; i++){
+	for(int i = 0; i < 10; i++){
 		dice.roll();
 		REQUIRE(dice.rolled_value() >= 1);
 		REQUIRE(dice.rolled_value() <= 6 );
@@ -31,7 +33,7 @@ TEST_CASE("Testing Roll Class:"){
 
 	Roll dicetoss(dice1, dice2);
 
-	for(int i = 0; i <= 10; i++){
+	for(int i = 0; i < 10; i++){
 			dicetoss.roll_die();
 		REQUIRE(dicetoss.roll_value() >= 2);
 		REQUIRE(dicetoss.roll_value() <= 12);
@@ -48,11 +50,47 @@ TEST_CASE("Testing Shooter Class:"){
 
 	Shooter diceroll;
 
-	for(int i = 0; i <= 10; i++){
-			;
+	for(int i = 0; i < 10; i++){
+			
 		REQUIRE(diceroll.throw_die(dice1, dice2) != NULL);
 		REQUIRE(diceroll.throw_die(dice1, dice2)->roll_value() >= 2);
 		REQUIRE(diceroll.throw_die(dice1, dice2)->roll_value() <= 12);
 	}
 
 }
+
+TEST_CASE("Testing ComeOutPhase Class:"){
+
+	srand(time(0));
+
+	Die dice1;
+	Die dice2;
+	Shooter diceroll;
+	ComeOutPhase phase;
+
+	for(int i = 0; i < 10; i++){
+
+		REQUIRE(phase.get_outcome(diceroll.throw_die(dice1, dice2)) != RollOutcome::seven_out);
+
+	}
+
+}
+
+TEST_CASE("Testing PointPhase Class:"){
+
+	srand(time(0));
+
+	Die dice1;
+	Die dice2;
+	Shooter diceroll;
+
+	for(int i = 0; i < 10; i++){
+
+		int points = rand() % 12 + 1;
+		PointPhase phase(points);
+
+		REQUIRE(phase.get_outcome(diceroll.throw_die(dice1, dice2)) != RollOutcome::natural);
+
+	}
+
+ }
